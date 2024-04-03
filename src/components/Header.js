ea984a862363dc97fa85c532276f7003adf8049b
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-03 22:15:15 +0530

    added a css file for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import "../css/Header.css"
 
 const Header = () => {
 return (
     <>
-      <Navbar expand="lg">
+      <div className='header_outermost_div'>
         <Container>
-          <Nav className="me-auto">
-            <NavDropdown title="Home" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#news">News</NavDropdown.Item>
-              <NavDropdown.Item href="#reports">Reports</NavDropdown.Item>
-              <NavDropdown.Item href="#action">Actions</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#home">About Us</Nav.Link>
-            <Nav.Link href="#home">Services</Nav.Link>
-            <Nav.Link href="#home">Pricing</Nav.Link>
-            <Nav.Link href="#home">FAQ</Nav.Link>
-            <Nav.Link href="#home">Contact</Nav.Link>
-          </Nav>
+          <Navbar expand="lg">
+            <Container>
+              <Nav className="me-auto">
+                <NavDropdown title="Home" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#news">News</NavDropdown.Item>
+                  <NavDropdown.Item href="#reports">Reports</NavDropdown.Item>
+                  <NavDropdown.Item href="#action">Actions</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#home">About Us</Nav.Link>
+                <Nav.Link href="#home">Services</Nav.Link>
+                <Nav.Link href="#home">Pricing</Nav.Link>
+                <Nav.Link href="#home">FAQ</Nav.Link>
+                <Nav.Link href="#home">Contact</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
         </Container>
-      </Navbar>
-
-    </>
+      </div>
+         </>
   );
 };
 
