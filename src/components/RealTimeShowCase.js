9fcebd00a81623d3ecbab2038b815073242fc86a
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-04 13:14:48 +0530

    added new component and its css file for displaying real time show case

---
+++ src/components/RealTimeShowCase.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RealTimeCom from "../css/RealTimeCom.css"
+const RealTimeShowCase = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+export default RealTimeShowCase
