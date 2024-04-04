698353f9ea817aa88126757972421d216e9459a2
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-04 11:39:00 +0530

    added swap globe and cloud icons

--- src/components/BenefitsDescription.js
+++ src/components/BenefitsDescription.js
@@ -1,10 +1,51 @@
 import React from "react";
+import "../css/BenefitsDescription.css"
+import { Row, Col } from 'react-bootstrap'
+import { FaGlobe } from 'react-icons/fa';
+import { BiTransfer } from 'react-icons/bi';
+// import { IoIosAddCircle } from 'react-icons/io';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 const BenefitsDescription = () => {
     return (
         <>
             <div>
-
+                <Row>
+                    <Col md={4} className="column">
+                        <div className="globe_icon center">
+                            <FaGlobe size={32} color="white" />
+                        </div>
+                        <div>
+                            <h2>Powerful Modern Idea</h2>
+                        </div>
+                        <div>
+                            <p>Drawings me opinions returned absolute in.Otherwise therefore are are unfeeling something.Certain be ye amiable</p>
+                        </div>
+                    </Col>
+                    <Col md={4} className="column">
+                        <div className="cloud_icon center">
+                            <AiOutlineCloudUpload strokeWidth={3} size={32} color="white" />
+                        </div>
+                        <div>
+                            <h2>Easy Data Analytics</h2>
+                        </div>
+                        <div>
+                            <p>Drawings me opinions returned absolute in.Otherwise therefore are are unfeeling something.Certain be ye amiable</p>
+                        </div>
+                    </Col>
+                    <Col md={4} className="column">
+                        <div className="swap_icon center">
+                            <BiTransfer strokeWidth={3} size={32} color="white" />
+                        </div>
+                        <div>
+                            <h2>Best Software Developer</h2>
+                        </div>
+                        <div>
+                            <p>Drawings me opinions returned absolute in.Otherwise therefore are are unfeeling something.Certain be ye amiable</p>
+                        </div>
+                    </Col>
+                </Row>
+                <Row></Row>
             </div>
         </>
     )
