218b1739068187d6040b8b74d6d43939e0497ac7
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-04 13:45:04 +0530

    described company status using div

--- src/components/CompanyStatus.js
+++ src/components/CompanyStatus.js
@@ -4,7 +4,28 @@ import "../css/Status.css"
 const CompanyStatus = () => {
     return (
         <>
-
+            <div className="outermost_div">
+                <h3>Best Growing Agency</h3>
+                <p>Savings her pleased are several started females met. short her not being any.</p>
+                <div className="center">
+                    <div>
+                        <p>100%</p>
+                        <p>Satisfaction</p>
+                    </div>
+                    <div>
+                        <p>+122K</p>
+                        <p>Clients</p>
+                    </div>
+                    <div>
+                        <p>55K</p>
+                        <p>properties</p>
+                    </div>
+                    <div>
+                        <p>3,587+</p>
+                        <p>Rooms</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
