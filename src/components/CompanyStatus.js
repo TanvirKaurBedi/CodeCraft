890f24779dcf33a471e1d2b6c507da4d1d4de67e
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-05 16:01:03 +0530

    changes in ui

--- src/components/CompanyStatus.js
+++ src/components/CompanyStatus.js
@@ -10,19 +10,19 @@ const CompanyStatus = () => {
                 <p className="subheader_text">Savings her pleased are several started females met. short her not being any.</p>
                 <div className="center">
                     <div>
-                        <p className="status_digits"><CountUp start={1} end={100} duration={5} />%</p>
+                        <p className="status_digits"><CountUp start={1} end={100} duration={9} />%</p>
                         <p>Satisfaction</p>
                     </div>
                     <div>
-                        <p className="status_digits">+<CountUp start={1} end={122} duration={6} />K</p>
+                        <p className="status_digits">+<CountUp start={1} end={122} duration={7} />K</p>
                         <p>Clients</p>
                     </div>
                     <div>
-                        <p className="status_digits"><CountUp start={1} end={55} duration={5} />K</p>
+                        <p className="status_digits"><CountUp start={1} end={55} duration={7} />K</p>
                         <p>properties</p>
                     </div>
                     <div>
-                        <p className="status_digits"><CountUp start={1} end={3587} duration={3} />+</p>
+                        <p className="status_digits"><CountUp start={1} end={3587} duration={7} />+</p>
                         <p>Rooms</p>
                     </div>
                 </div>
